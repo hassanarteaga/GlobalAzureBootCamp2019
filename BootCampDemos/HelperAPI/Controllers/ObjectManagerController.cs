38f42832e04f5bb9e7bd13e5edff95148188eaac
hassanarteaga <hassan.arteaga@gmail.com>
2019-05-07 09:51:33 -0500

    Rev 12.1 Model Validation

--- BootCampDemos/HelperAPI/Controllers/ObjectManagerController.cs
+++ BootCampDemos/HelperAPI/Controllers/ObjectManagerController.cs
@@ -7,7 +7,6 @@ using System.Threading.Tasks;
 using HelperAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace HelperAPI.Controllers
 {
